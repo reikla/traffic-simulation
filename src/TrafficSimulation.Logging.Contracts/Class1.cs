891c49e5bede3e84f1e72899e56df38272619ce9
reimarklammer <reimar_klammer@gmx.at>
2017-04-10 20:27:01 +0200

    Projekte angelegt

---
+++ src/TrafficSimulation.Logging.Contracts/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficSimulation.Logging.Contracts
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/TrafficSimulation.Logging.LogServer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficSimulation.Logging.LogServer
+{
+    public class Class1
+    {
+    }
+}
