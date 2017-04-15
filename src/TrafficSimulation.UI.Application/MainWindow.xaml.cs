e44e95e55748fa0bc50297f03c41bb6f82ad9d6a
Tobias Mayer <tobias.mayer@neumarkt.at>
2017-04-16 00:56:53 +0200

    UI-Spielereien

--- src/TrafficSimulation.UI.Application/MainWindow.xaml.cs
+++ src/TrafficSimulation.UI.Application/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
 
 namespace TrafficSimulation.UI.Application
 {
@@ -12,6 +16,31 @@ namespace TrafficSimulation.UI.Application
       InitializeComponent();
       DataContext = new ViewModelBase();
       
+    }
+
+    private void Button1_Click(object sender, RoutedEventArgs e)
+    {
+     
+
+
+      DoubleAnimation da = new DoubleAnimation();
+      da.Duration = new Duration(TimeSpan.FromSeconds(10));
+      da.To = 300;
+      da.From = 0;
+      da.RepeatBehavior = RepeatBehavior.Forever;
+      da.EasingFunction = new BounceEase();
+      TranslateTransform tt = new TranslateTransform();
+      rectangle1.RenderTransform = tt;
+      tt.BeginAnimation(TranslateTransform.XProperty, da);
+    
+
+
+
+    }
+
+    private void MainWindow_OnSizeChanged(object sender, SizeChangedEventArgs e)
+    {
+      rectangle1.
     }
   }
 }
