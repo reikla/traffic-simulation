7b84a2c8f7f01cbe7236366632a7939699d367f8
Tobias Mayer <tobias.mayer@neumarkt.at>
2017-04-16 21:27:31 +0200

    Ui für Sprint 1 -> Auto fährt auf Straße immer wieder hin und her

--- src/TrafficSimulation.UI.Application/MainWindow.xaml.cs
+++ src/TrafficSimulation.UI.Application/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
+using System.Windows.Shapes;
 
 namespace TrafficSimulation.UI.Application
 {
@@ -11,36 +12,52 @@ namespace TrafficSimulation.UI.Application
   /// </summary>
   public partial class MainWindow : Window
   {
+    bool animation_start = false;
     public MainWindow()
     {
       InitializeComponent();
       DataContext = new ViewModelBase();
+      Rectangle car = new Rectangle();
+      car.Fill = Brushes.Black;
+      car.Width = 100;
+      car.Height = 50;
+
+      Canvas canvas = new Canvas();
+      canvas.Width = 500;
+      canvas.Height = 500;
+      canvas.Background = Brushes.LightGray;
+      Rectangle street = new Rectangle();
+      street.Fill = Brushes.SlateGray;
+      canvas.Children.Add(street);
+      canvas.Children.Add(car);
       
-    }
 
-    private void Button1_Click(object sender, RoutedEventArgs e)
-    {
+      this.Content = canvas;
+      UpdateLayout();
      
-
-
-      DoubleAnimation da = new DoubleAnimation();
-      da.Duration = new Duration(TimeSpan.FromSeconds(10));
-      da.To = 300;
-      da.From = 0;
-      da.RepeatBehavior = RepeatBehavior.Forever;
-      da.EasingFunction = new BounceEase();
-      TranslateTransform tt = new TranslateTransform();
-      rectangle1.RenderTransform = tt;
-      tt.BeginAnimation(TranslateTransform.XProperty, da);
-    
-
-
-
     }
+
 
     private void MainWindow_OnSizeChanged(object sender, SizeChangedEventArgs e)
     {
-      rectangle1.
+      UpdateLayout();
+      MainWindow mw = sender as MainWindow;
+      Canvas cv = mw.Content as Canvas;
+      cv.Height = mw.ActualHeight;
+      cv.Width = mw.ActualWidth;
+      Rectangle street = cv.Children[0] as Rectangle;
+      Rectangle car = cv.Children[1] as Rectangle;
+      street.Width = mw.ActualWidth;
+      street.Height = car.Height*2;
+      Canvas.SetTop(street, mw.ActualHeight / 2);
+      Canvas.SetTop(car, Canvas.GetTop(street) + street.Height - car.Height);
+      TranslateTransform tt = new TranslateTransform();
+      car.RenderTransform = tt;
+      DoubleAnimation da = new DoubleAnimation(0, mw.ActualWidth/*-car.Width*/, TimeSpan.FromSeconds(20));
+      da.RepeatBehavior = RepeatBehavior.Forever;
+      //da.EasingFunction = new SineEase();
+      tt.BeginAnimation(TranslateTransform.XProperty, da);
+   
     }
   }
 }
