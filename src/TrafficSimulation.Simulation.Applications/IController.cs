a25898ed192113a6cc6f6f599f8eb87e89ba1278
reimarklammer <reimar_klammer@gmx.at>
2017-04-23 16:38:22 +0200

    added application class

---
+++ src/TrafficSimulation.Simulation.Applications/IController.cs
@@ -0,0 +1,17 @@
+﻿namespace TrafficSimulation.Simulation.Applications
+{
+  /// <summary>
+  /// The controller of a part of the application
+  /// </summary>
+  internal interface IController
+  {
+    /// <summary>
+    /// Starts a part of the application
+    /// </summary>
+    void Start();
+    /// <summary>
+    /// Stops a part of the Application
+    /// </summary>
+    void Shutdown();
+  }
+}
---
+++ src/TrafficSimulation.Simulation.Applications/Program.cs
@@ -0,0 +1,13 @@
+﻿using TrafficSimulation.Simulation.Engine;
+namespace TrafficSimulation.Simulation.Applications
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var engine = new SimulationEngine();
+
+
+    }
+  }
+}
