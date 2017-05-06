4fe082d8c0766e4a0f10c48bc894f1e3b5e05707
Tobias Mayer <tobias.mayer@neumarkt.at>
2017-05-06 08:23:44 +0200

    changed the formular to reimars proposal - has issues :(

--- src/TrafficSimulation.UI.Application/MainWindow.xaml.cs
+++ src/TrafficSimulation.UI.Application/MainWindow.xaml.cs
@@ -94,39 +94,17 @@ namespace TrafficSimulation.UI.Application
         foreach (var viewModelVehicle in ViewModel.Vehicles)
         {
           var rectangle = DrawVehicle(10, 10);
-          NodeConnection street = ViewModel.NodeConnections.First(x => x.Id == viewModelVehicle.CurrentNodeConnectionId);
-          Node startNode = ViewModel.Nodes.First(x => x.Id == street.StartNodeId);
-          Node endNode = ViewModel.Nodes.First(x => x.Id == street.EndNodeId);
+          NodeConnection street = ViewModel.NodeConnections.First(nc => nc.Id == viewModelVehicle.CurrentNodeConnectionId);
+          Node startNode = ViewModel.Nodes.First(n => n.Id == street.StartNodeId);
+          Node endNode = ViewModel.Nodes.First(n => n.Id == street.EndNodeId);
 
           MainCanvas.Children.Add(rectangle);
-
-          //double a = startNode.X * MainCanvas.ActualWidth;
-          //double b = startNode.Y * MainCanvas.ActualHeight;
-          //double c = endNode.X * MainCanvas.ActualWidth;
-          //double d = endNode.Y * MainCanvas.ActualHeight;
-
-          //double g = Math.Sqrt(Math.Pow(a-c,2)+Math.Pow(b-d,2));
-          //double h = viewModelVehicle.PositionOnConnection;
-          //double k = street.Length;
-
-          double x01 = ((startNode.X * MainCanvas.ActualWidth * Math.Pow(endNode.Y * MainCanvas.ActualHeight, 2) - 2 * startNode.X * MainCanvas.ActualWidth * startNode.Y * MainCanvas.ActualHeight * endNode.Y * MainCanvas.ActualHeight + startNode.X * MainCanvas.ActualWidth * Math.Pow(endNode.X * MainCanvas.ActualWidth, 2) - 2 * Math.Pow(startNode.X * MainCanvas.ActualWidth, 2) * endNode.X * MainCanvas.ActualWidth +
-                         startNode.X * MainCanvas.ActualWidth * Math.Pow(startNode.Y * MainCanvas.ActualHeight, 2) + Math.Pow(startNode.X * MainCanvas.ActualWidth, 3)) * street.Length
-                        + (endNode.X * MainCanvas.ActualWidth - startNode.X * MainCanvas.ActualWidth) * Math.Sqrt(Math.Pow(endNode.Y * MainCanvas.ActualHeight, 2) - 2 * startNode.Y * MainCanvas.ActualHeight * endNode.Y * MainCanvas.ActualHeight + Math.Pow(endNode.X * MainCanvas.ActualWidth, 2) - 2 * startNode.X * MainCanvas.ActualWidth * endNode.X * MainCanvas.ActualWidth + Math.Pow(startNode.Y * MainCanvas.ActualHeight, 2) +
-                                              Math.Pow(startNode.X * MainCanvas.ActualWidth, 2)) * Math.Sqrt(Math.Pow(startNode.X * MainCanvas.ActualWidth - endNode.X * MainCanvas.ActualWidth, 2) + Math.Pow(startNode.Y * MainCanvas.ActualHeight - endNode.Y * MainCanvas.ActualHeight, 2)) * viewModelVehicle.PositionOnConnection) /
-                       ((Math.Pow(endNode.Y * MainCanvas.ActualHeight, 2) - 2 * startNode.Y * MainCanvas.ActualHeight * endNode.Y * MainCanvas.ActualHeight + Math.Pow(endNode.X * MainCanvas.ActualWidth, 2) - 2 * startNode.X * MainCanvas.ActualWidth * endNode.X * MainCanvas.ActualWidth + Math.Pow(startNode.Y * MainCanvas.ActualHeight, 2) + Math.Pow(startNode.X * MainCanvas.ActualWidth, 2)) *
-                        street.Length);
-
-          //double x02 = ((-a * Math.Pow(d, 2) + 2 * a * b * d - a * Math.Pow(c, 2) + 2 * Math.Pow(a, 2) * c -
-          //               a * Math.Pow(b, 2) - Math.Pow(a, 3)) * k
-          //              + (c - a) * Math.Sqrt(Math.Pow(d, 2) - 2 * b * d + Math.Pow(c, 2) - 2 * a * c + Math.Pow(b, 2) +
-          //                                    Math.Pow(a, 2)) * g * h) /
-          //             ((Math.Pow(d, 2) - 2 * b * d + Math.Pow(c, 2) - 2 * a * c + Math.Pow(b, 2) + Math.Pow(a, 2)) *
-          //              k);
-          double y01 = ((startNode.Y * MainCanvas.ActualHeight - endNode.Y * MainCanvas.ActualHeight) / (startNode.X * MainCanvas.ActualWidth - endNode.X * MainCanvas.ActualWidth)) * (x01 - startNode.X * MainCanvas.ActualWidth) + startNode.Y * MainCanvas.ActualHeight;
-          //double y02 = ((b - d) / (a - c)) * (x02 - a) + b;
-
-          Canvas.SetLeft(rectangle, x01);
-          Canvas.SetTop(rectangle, y01 - rectangle.Height/2);
+          var deltaX = endNode.X - startNode.X;
+          var deltaY = endNode.Y - startNode.Y;
+          var x = endNode.X * deltaX * (viewModelVehicle.PositionOnConnection / street.Length) + startNode.X;
+          var y = endNode.Y * deltaY * (viewModelVehicle.PositionOnConnection / street.Length) + startNode.Y;
+          Canvas.SetLeft(rectangle, x*MainCanvas.ActualWidth);
+          Canvas.SetTop(rectangle, y*MainCanvas.ActualHeight );
 
         }
       }
