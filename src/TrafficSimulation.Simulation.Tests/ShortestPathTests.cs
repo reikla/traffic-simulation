48282bbdfcf3a4a09e5457a381fb7e407ae59884
reimarklammer <reimar_klammer@gmx.at>
2017-05-31 22:14:05 +0200

    reverted last checkins...

--- src/TrafficSimulation.Simulation.Tests/ShortestPathTests.cs
+++ src/TrafficSimulation.Simulation.Tests/ShortestPathTests.cs
@@ -13,7 +13,9 @@ namespace TrafficSimulation.Simulation.Tests
     [TestMethod]
     public void OutputShortestPath()
     {
-    
+      List<Node> AllNodes = new List<Node>();
+      List<NodeConnection> AllNodeConnections = new List<NodeConnection>();
+
       XmlGraphReader gr = new XmlGraphReader();
 
       gr.Read("test1.graphml");
@@ -25,6 +27,30 @@ namespace TrafficSimulation.Simulation.Tests
       var sp = new ShortestPath(gr.NodeConnections, gr.Nodes, startNode.Id, endNode.Id);
 
      
+
+
+
+
+
+
+//      AllNodes.Add(new Node(0,1));
+//      AllNodes.Add(new Node(1));
+//      AllNodes.Add(new Node(2));
+//      AllNodes.Add(new Node(3));
+//      AllNodes.Add(new Node(4));
+//      AllNodes.Add(new Node(5));
+//
+//      AllNodeConnections.Add(new NodeConnection(1, 1, 2, 10d));
+//      AllNodeConnections.Add(new NodeConnection(2, 1, 3, 30d));
+//      AllNodeConnections.Add(new NodeConnection(3, 2, 4, 3d));
+//      AllNodeConnections.Add(new NodeConnection(4, 3, 5, 5d));
+//      AllNodeConnections.Add(new NodeConnection(5, 5, 4, 30d));
+//
+//      int StartNodeId = 1;
+//      int EndNodeId = 4;
+//
+//
+//      var sp = new ShortestPath(AllNodeConnections,AllNodes,StartNodeId,EndNodeId);
     }
   }
 }
