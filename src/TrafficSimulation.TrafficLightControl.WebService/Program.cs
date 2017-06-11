1270d6cb8695465178585930d339df9ebea5baea
Tobias Mayer <tobias.mayer@neumarkt.at>
2017-06-11 17:33:30 +0200

    removed the Starting of the Engine WebService within TrafficSimulation.TrafficLightControl.WebService

--- src/TrafficSimulation.TrafficLightControl.WebService/Program.cs
+++ src/TrafficSimulation.TrafficLightControl.WebService/Program.cs
@@ -8,15 +8,6 @@ namespace TrafficSimulation.TrafficLightControl.WebService
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Press any key to start simulation");
-      Console.ReadKey();
-      var binding = new NetNamedPipeBinding(NetNamedPipeSecurityMode.Transport);
-      var ep = new EndpointAddress("net.pipe://localhost/Simulation/Engine");
-      var simulationService = ChannelFactory<ISimulationService>.CreateChannel(binding, ep);
-
-
-
-      simulationService.Start();
 
       Console.WriteLine("Traffic light Control Webservice Started. Press any key to exit.");
       Console.ReadKey();
